477015e8ac2eb967e4ddeeb7501f7447dd02b3a4
Bytewaiser <bytewaiser@gmail.com>
2023-05-29 23:56:57 +0300

    8.Flag started

---
+++ 8.Stonks/netcat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    // Make a system call to the netcat program.
+    system("nc mercury.picoctf.net 6989");
+
+}
---
+++ 8.Stonks/readme.md
@@ -0,0 +1,10 @@
+Get ip of `mercury.picoctf.net` with `ping`
+
+ip: 18.189.209.142
+
+Use netcat to see open ports
+```
+    nc -v -n 18.189.209.142 1-1000
+```
+
+Port 22 is open (ssh)
